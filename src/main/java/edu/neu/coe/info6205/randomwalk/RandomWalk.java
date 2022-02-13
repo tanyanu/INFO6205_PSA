71dbff2f53f42026a4a1871cdd700a433a7224a4
tanyanu <shah.tany@northeastern.edu>
2022-02-12 19:22:09 -0500

    Base repo + Assignment 2 - benchmark

---
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017. Phasmid Software
+ */
+
+package edu.neu.coe.info6205.randomwalk;
+
+import java.util.Random;
+
+public class RandomWalk {
+
+    private int x = 0;
+    private int y = 0;
+
+    private final Random random = new Random();
+
+    /**
+     * Private method to move the current position, that's to say the drunkard moves
+     *
+     * @param dx the distance he moves in the x direction
+     * @param dy the distance he moves in the y direction
+     */
+    private void move(int dx, int dy) {
+        // FIXME do move by replacing the following code
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+        // END
+
+    }
+
+    /**
+     * Perform a random walk of m steps
+     *
+     * @param m the number of steps the drunkard takes
+     */
+    private void randomWalk(int m) {
+        // FIXME
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
+
+        // END 
+    }
+
+    /**
+     * Private method to generate a random move according to the rules of the situation.
+     * That's to say, moves can be (+-1, 0) or (0, +-1).
+     */
+    private void randomMove() {
+        boolean ns = random.nextBoolean();
+        int step = random.nextBoolean() ? 1 : -1;
+        move(ns ? step : 0, ns ? 0 : step);
+    }
+
+    /**
+     * Method to compute the distance from the origin (the lamp-post where the drunkard starts) to his current position.
+     *
+     * @return the (Euclidean) distance from the origin to the current position.
+     */
+    public double distance() {
+        int origin = 0;
+
+        double xCoUnit =  Math.abs(this.x - origin);
+        double yCoUnit =  Math.abs(this.y - origin);
+
+        return Math.sqrt((xCoUnit * xCoUnit) + (yCoUnit * yCoUnit));
+    }
+
+    /**
+     * Perform multiple random walk experiments, returning the mean distance.
+     *
+     * @param m the number of steps for each experiment
+     * @param n the number of experiments to run
+     * @return the mean distance
+     */
+    public static double randomWalkMulti(int m, int n) {
+        double totalDistance = 0;
+        for (int i = 0; i < n; i++) {
+            RandomWalk walk = new RandomWalk();
+            walk.randomWalk(m);
+            totalDistance = totalDistance + walk.distance();
+        }
+        return totalDistance / n;
+    }
+
+    public static void main(String[] args) {
+        int[] stepsArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ,19, 20};
+        int n = 10;
+
+        //Loop over steps array to get no of steps for each experiment
+        for (int m = 1; m < stepsArr.length; m++) {
+            double meanDistance = randomWalkMulti(m, n);
+            System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        }
+    }
+
+}
---
+++ src/test/java/edu/neu/coe/info6205/sort/elementary/BenchmarkInsertionSort.java
@@ -0,0 +1,63 @@
+package edu.neu.coe.info6205.sort.elementary;
+
+import edu.neu.coe.info6205.sort.BaseHelper;
+import edu.neu.coe.info6205.sort.Helper;
+import edu.neu.coe.info6205.util.ConfigTest;
+import edu.neu.coe.info6205.sort.SortWithHelper;
+import edu.neu.coe.info6205.util.Benchmark;
+import edu.neu.coe.info6205.util.Benchmark_Timer;
+
+import java.io.IOException;
+
+public class BenchmarkInsertionSort {
+
+    public static void main(String[] args) throws IOException {
+
+        int N = 200;
+
+        for (int i = 0; i <= 5; i++) {
+            while (N <= 1600) {
+                Helper<Integer> helper = new BaseHelper<>("Insertion sort", N,
+                        ConfigTest.setupConfig("true", "0", "1", "", ""));
+                SortWithHelper<Integer> arrSorter = new InsertionSort<>(helper);
+
+                Benchmark<Integer[]> benchmarkTimer = new Benchmark_Timer<Integer[]>("Insertion Sort",
+                        b -> arrSorter.sort(b));
+                Integer[] randomArr = helper.random(Integer.class, r -> r.nextInt(1000));
+
+                System.out.println("************************************************************");
+                System.out.println("\n");
+
+                double rndmRunningTime = benchmarkTimer.run(randomArr, N);
+                System.out.println("(N) -> " + randomArr.length);
+                System.out.println("Running Time Unsorted random elements : " + rndmRunningTime);
+
+                System.out.println("************************************************************");
+                System.out.println("\n");
+                System.out.println("(N) -> " + randomArr.length);
+                Integer[] ascArr = arrSorter.sort(randomArr);
+                double ascRunningTime = benchmarkTimer.run(ascArr, N);
+                System.out.println("Running Time ASC sorted : " + ascRunningTime);
+
+                System.out.println("************************************************************");
+                System.out.println("\n");
+                arrSorter.sort(ascArr, ascArr.length - 1, 0);
+                System.out.println("(N) -> " + ascArr.length);
+                double descRunningOTime = benchmarkTimer.run(ascArr, N);
+                System.out.println("Running Time DESC sorted: " + descRunningOTime);
+
+                System.out.println("************************************************************");
+                System.out.println("\n");
+                arrSorter.sort(ascArr, 0, (ascArr.length) / 3);
+                System.out.println("(N) -> " + ascArr.length);
+                double partialRunningTime = benchmarkTimer.run(ascArr, N);
+                System.out.println("Running Time Partially sorted : " + partialRunningTime);
+
+                System.out.println("************************************************************");
+                N = N + 00;
+            }
+        }
+
+    }
+
+}
